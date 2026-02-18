e478a2a23361938ad94eb25f0bc30013a15eafd2
VentsislavX <karausovventsislav@gmail.com>
2026-02-19 00:40:54 +0200

    Initial commit

---
+++ src/piece/Piece.java
@@ -0,0 +1,75 @@
+package piece;
+
+import main.Board;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Piece {
+    public BufferedImage image;
+    public int x, y ;
+    public int col, row, preCol, preRow;
+    public int color;
+
+    public Piece(int color, int col, int row) {
+        this.color = color;
+        this.col = col;
+        this.row = row;
+        x = getX(col);
+        y = getY(row);
+        preCol = col;
+        preRow = row;
+    }
+
+    public BufferedImage getImage(String imagePath) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
+        }
+        catch(IOException e) {
+                e.printStackTrace();
+        }
+        return image;
+    }
+
+    public int getX(int col) {
+        return col * Board.SQUARE_SIZE;
+    }
+
+    public int getY(int row) {
+        return row * Board.SQUARE_SIZE;
+    }
+
+    public int getCol(int x) {
+        return (x + Board.HALF_SQUARE_SIZE) / Board.SQUARE_SIZE;
+    }
+
+    public int getRow(int y) {
+        return (y + Board.HALF_SQUARE_SIZE) / Board.SQUARE_SIZE;
+    }
+
+    public void updatePositon() {
+        x = getX(col);
+        y = getY(row);
+        preCol = getCol(x);
+        preRow = getRow(y);
+    }
+
+    public boolean canMove(int targetCol, int targetRow) {
+        return false;
+    }
+
+    public boolean isWithinTheBoard(int targetCol, int targetRow) {
+        if(targetCol >= 0 && targetCol <= 7 && targetRow >= 0 && targetRow <= 7) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void draw(Graphics2D g2) {
+        g2.drawImage(image, x, y, Board.SQUARE_SIZE, Board.SQUARE_SIZE, null);
+    }
+}
